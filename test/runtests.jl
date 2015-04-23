edf7cdb198c287af282755a0ecdfde383f55a036
Douglas Bates <dmbates@gmail.com>
2015-04-23 11:01:30 -0500

    Include and use data for tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,10 @@
-using ReTerms, DataArrays, PDMats
+using DataArrays, DataFrames, PDMats, ReTerms
 using Base.Test
 
-const Batch = compact(@pdata(rep('A':'F', 1, 5)))
-const sf = ScalarReTerm(Batch)
-const Yield = [1545.,1440.,1440.,1520.,1580.,1540.,1555.,1490.,1560.,1495.,
-               1595.,1550.,1605.,1510.,1560.,1445.,1440.,1595.,1465.,1545.,
-               1595.,1630.,1515.,1635.,1625.,1520.,1455.,1450.,1480.,1445.]
+include(joinpath(dirname(@__FILE__),"data.jl"))
+
+const sf = ScalarReTerm(ds[:Batch])
+const Yield = convert(Vector{Float64}, array(ds[:Yield]))
 
 @test size(sf) == (30, 6)
 @test size(sf,1) == 30
