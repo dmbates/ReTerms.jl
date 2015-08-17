d768d517ab7ed90caf6d460423c763a811901c6a
Douglas Bates <bates@stat.wisc.edu>
2015-08-17 16:52:39 -0500

    Add tests.

--- test/scalarReTerm.jl
+++ test/scalarReTerm.jl
@@ -1,5 +1,5 @@
 const sf = ReMat(ds[:Batch])
-const Yield = convert(Vector{Float64}, convert(Array,ds[:Yield]))
+const Yield = convert(Vector{Float64},ds[:Yield])
 
 @test size(sf) == (30, 6)
 @test size(sf,1) == 30
@@ -17,3 +17,8 @@ const rhs = sf'Yield
 @test rhs == [7525.0,7640.0,7820.0,7490.0,8000.0,7350.0]
 @test A_ldiv_B!(similar(rhs),crp,rhs) == [1505.,1528.,1564.,1498.,1600.,1470.]
 
+const L = ReTerms.ColMajorLowerTriangular(1)
+L[:θ] = [0.5]
+
+@test isa(Ac_mul_B!(L,crp),ReTerms.HBlkDiag)
+@test crp.arr == fill(2.5,(1,1,6))
