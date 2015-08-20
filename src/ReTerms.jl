1a12cc4439395cf0b602bf3998bf48c74d280769
Douglas Bates <bates@stat.wisc.edu>
2015-08-20 16:15:31 -0500

    Reindent

--- src/ReTerms.jl
+++ src/ReTerms.jl
@@ -1,16 +1,21 @@
+VERSION >= v"0.4.0-dev+6521" && __precompile__()
+
 module ReTerms
 
-    using DataArrays, DataFrames, HDF5, NLopt, StatsBase
+using DataArrays, DataFrames, HDF5, NLopt, StatsBase
 
-    export ReMat
+export ReMat
 
-    export g2dict, lowerbd, objective, reterm
+export g2dict, lowerbd, objective, reterm
 
-    using Base.LinAlg.BlasInt
+using Base.LinAlg.BlasInt
 
-    include("blockmats.jl")
-    include("remat.jl")
-    include("paramlowertriangular.jl")
-    include("pls.jl")
+import Base: ==
+
+include("utils.jl")
+include("blockmats.jl")
+include("remat.jl")
+include("paramlowertriangular.jl")
+include("pls.jl")
 
 end # module
